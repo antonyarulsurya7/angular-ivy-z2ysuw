af0493069605fc786417677edaa6a780cd2b8b1e
antonyarulsurya7 <antonyarulsurya@gmail.com>
2021-08-06 11:57:45 +0530

    3 files modified

--- src/app/hello.component.ts
+++ src/app/hello.component.ts
@@ -1,10 +1,27 @@
 import { Component, Input } from '@angular/core';
+import { Http } from '@angular/http';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/operator/map';
 
 @Component({
   selector: 'hello',
-  template: `<h1>Hello {{name}}!</h1>`,
-  styles: [`h1 { font-family: Lato; }`]
+  template: `
+    <h1>Hello {{ name }}!</h1>
+  `,
+  styles: [
+    `
+      h1 {
+        font-family: Lato;
+      }
+    `
+  ]
 })
-export class HelloComponent  {
+export class HelloComponent {
   @Input() name: string;
+  constructor(private http: Http) {}
+  getMovie(): Observable<any> {
+    return this.http
+      .get('http://www.omdbapi.com/?i=tt3896198&apikey=5b8ebe9f')
+      .map((response: any) => response.json);
+  }
 }
