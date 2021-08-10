d3338cba1dc0c6c8d3bd3dbe8bed3282544c8d1c
antonyarulsurya7 <antonyarulsurya@gmail.com>
2021-08-10 20:49:33 +0530

    1 file modified

--- src/app/movie.service.ts
+++ src/app/movie.service.ts
@@ -1,18 +1,14 @@
 import { Injectable } from '@angular/core';
-import { Http, Response } from '@angular/http';
+import { Http,Response } from '@angular/http';
 import { Observable } from 'rxjs';
-import { catchError, map } from 'rxjs/operators';
 
 @Injectable()
 export class MovieService {
   constructor(private http: Http) {}
 
   getMovie(): Observable<any> {
-    return this.http
-      .get('http://www.7timer.info/bin/api.pl?lon=113.17&lat=23.09&product=astro&output=json')
-      .pipe(
-        map((response: Response) => response.json)
-      );
+    return this.http.get(
+      'http://www.7timer.info/bin/api.pl?lon=113.17&lat=23.09&product=astro&output=json'
+    ).pipe(map(response: Response));
   }
- 
 }
